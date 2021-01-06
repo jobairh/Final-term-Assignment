14a3954d9799decf0cc4237a06b9919d270d40dc
jobairh <jobairh999@gmail.com>
2021-01-06 11:06:03 +0600

    FinalAssignment done

---
+++ FinalAssignment/FinalAssignment/Models/Database.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalAssignment.Models
+{
+    public class Database
+    {
+        public Books Books { get; set; }
+
+        public Database()
+        {
+            string connString = "Server=DESKTOP-HE0MQ0Q\\SQLEXPRESS;Integrated Security=true;Database=book_m_s";
+            SqlConnection conn = new SqlConnection(connString);
+            Books = new Books(conn);
+        }
+    }
+}
